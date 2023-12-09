94763f3c06ccd6ba27437d6d91384bb03125e908
BabyBroidy <152689141+BabyBroidy@users.noreply.github.com>
2023-12-09 13:45:43 +0300

    Add files via upload

---
+++ scripts/app.py
@@ -0,0 +1,29 @@
+import pygame
+import os
+from scripts.game import Game
+class App():
+    def __init__ (self):
+        self.maxfps= 60
+        self.display_size=(480,720)
+        self.running= True
+        self.display= pygame.display.set_mode(self.display_size)
+        self.clock=pygame.time.Clock()
+        self.game = Game()
+        pygame.display.set_caption('Doodle Jump')
+        pygame.display.set_icon(pygame.image.load(os.path.join('assets','icons','icon.ico')))
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                self.running = False
+    def update(self):
+        ...
+    def render(self):
+        self.fill.display((0,0,0)) #Закрашиваем окно
+        self.game.render(self.display)
+        pygame.display.update #Обновляем окно
+    def run(self):
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.render()
+
